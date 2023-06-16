85e19eca5e2319f0534b0cef812950eea1ea9261
Tortuga <tortuga0x0000@proton.me>
2023-06-16 09:46:34 +0200

    init bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world")
+import TelegramBot from 'node-telegram-bot-api';
+
+require('dotenv').config()
+
+const HTTP_API_TOKEN = process.env["HTTP_API_TOKEN"];
+
+if (!HTTP_API_TOKEN) {
+    throw new Error("Need to set a .env file with HTTP_API_TOKEN as the TG bot token")
+}
+
+const bot = new TelegramBot(HTTP_API_TOKEN, {polling: true});
