731fbae148b214cb0ef0f11241ce9f76988f890d
Tortuga <tortuga0x0000@proton.me>
2023-11-13 17:01:44 +0100

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git clone https://github.com/dagatsoin/tg-ticker-listing.git jared-bot
 cd jared-bot
 npm i
 npm run build
-cp .dist/* ../runtime/jared-bot
+cp ./dist/* ../runtime/jared-bot
 cd ../runtime/jared-bot
 node index.js
